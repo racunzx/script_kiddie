37da366c37b8fdecd63a89fe006bf5df2810aa3d
racunzx <88345813+racunzx@users.noreply.github.com>
2021-09-11 13:52:46 +0800

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 clear
-echo -e "=================================================" | lolcat
-echo -e "#     Premium Auto Script By Brody           #" | lolcat
-echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
-echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
-echo -e "# For VPS with KVM and VMWare virtualization    #" | lolcat
-echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# Thanks To  :                                  #" | lolcat
-echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# ALLAH S.W.T                                   #" | lolcat
-echo -e "# My Family                                     #" | lolcat
-echo -e "# Google                                        #" | lolcat
-echo -e "# Admin Portal & White Vps                      #" | lolcat
-echo -e "#                                               #" | lolcat
-echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# Rebuild By : BRODY                        #" | lolcat
-echo -e "=================================================" | lolcat
+echo -e "================================================="
+echo -e "#     Premium Auto Script By Brody           #" 
+echo -e "#-----------------------------------------------#" 
+echo -e "# For Debian 9 & Debian 10 64 bit               #" 
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" 
+echo -e "# For VPS with KVM and VMWare virtualization    #" 
+echo -e "#-----------------------------------------------#" 
+echo -e "# Thanks To  :                                  #" 
+echo -e "#-----------------------------------------------#" 
+echo -e "# ALLAH S.W.T                                   #" 
+echo -e "# My Family                                     #" 
+echo -e "# Google                                        #" 
+echo -e "# Admin Portal & White Vps                      #" 
+echo -e "#                                               #" 
+echo -e "#-----------------------------------------------#" 
+echo -e "# Rebuild By : BRODY                        #" 
+echo -e "=================================================" 
